e0e3e9bfea4cf19db237acd65bb9de321bfe09fa
tdespatie <tylerdespatie@cmail.carleton.ca>
2017-11-12 15:03:04 -0500

    Initial Commit

---
+++ MyTunesCollection.h
@@ -0,0 +1,92 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                   */
+/*  Program:  MyTunes Music Player                   */
+/*  Author:   Louis Nel								 */
+/*  Contributors: Tyler Despatie 101010622			 */
+/*  Date:     21-SEP-2017                            */
+/*                                                   */
+/*  (c) 2017 Louis Nel                               */
+/*  All rights reserved.  Distribution and           */
+/*  reposting, in part or in whole, requires         */
+/*  written consent of the author.                   */
+/*                                                   */
+/*  COMP 2404 students may reuse this content for    */
+/*  their course assignments without seeking consent */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+#ifndef ANSWER_CODE_MYTUNESCOLLECTION_H
+#define ANSWER_CODE_MYTUNESCOLLECTION_H
+
+#include "UI.h"
+#include <typeinfo>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class MyTunesCollection {
+public:
+    ~MyTunesCollection() {
+        for (int i=0; i<collection.size(); i++)
+            delete collection[i];
+    }
+
+    T * findByID(const int& anID) {
+        for (typename vector<T *>::iterator it = collection.begin(); it != collection.end(); ++it)
+            if ((*it)->getID() == anID) return *it;
+        return NULL;
+    }
+
+    T * findByID(const string& anID) {
+        for (typename vector<T*>::iterator itr = collection.begin() ; itr != collection.end(); ++itr)
+            if((*itr)->getUserID() == anID) return *itr;
+    }
+
+    void add(T &item) {
+        collection.push_back(&item);
+    }
+    void remove(T &item) {
+        typename vector<T*>::iterator index = findPosition(item);
+        if (index != collection.end()) {
+            T * theItem = *index;
+            collection.erase(index);
+            delete theItem;
+        }
+    }
+    void showOn(UI & aView) {
+        aView.printOutput("Print:");
+        for (int i=0; i<collection.size(); i++)
+            aView.printOutput((*collection[i]).toString());
+    }
+
+    // Refactored CODE
+    template<typename V>
+    void showOn(UI & aView, V memberID) {
+        const char *type = typeid(T).name();
+        aView.printOutput(*type + ":");
+        T * item = findByID(memberID);
+        if (item != NULL)
+            aView.printOutput(item->toString());
+    }
+
+    friend std::ostream& operator<<(ostream& out, const T& t) {
+        return out << t.toString() << endl;
+    }
+
+    vector<T*> getCollection() {
+        return collection;
+    }
+
+private:
+    vector<T*> collection;
+    typename vector<T*>::iterator findPosition(T &item) {
+        for(typename vector<T*>::iterator it = collection.begin(); it != collection.end(); ++it)
+            if (*it == &item) return it;
+        return collection.end();
+    }
+
+};
+
+#endif //ANSWER_CODE_MYTUNESCOLLECTION_H
---
+++ user.cpp
@@ -0,0 +1,98 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                   */
+/*  Program:  MyTunes Music Player                   */
+/*  Author:   Louis Nel								 */
+/*  Contributors: Tyler Despatie 101010622			 */
+/*  Date:     21-SEP-2017                            */
+/*                                                   */
+/*  (c) 2017 Louis Nel                               */
+/*  All rights reserved.  Distribution and           */
+/*  reposting, in part or in whole, requires         */
+/*  written consent of the author.                   */
+/*                                                   */
+/*  COMP 2404 students may reuse this content for    */
+/*  their course assignments without seeking consent */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <iostream>
+using namespace std;
+
+#include "user.h"
+
+int User::nextUserNumericID = 1000;
+	
+User::User(const string & aUserID, const string & aName){
+	cout << "User(string&, string&, int)" << endl;
+	userid = aUserID;
+	name = aName;
+	id = nextUserNumericID++;
+}
+User::User(const User & aUser){
+	cout << "User(const User & aUser)" << endl;
+	cout << "ERROR: User(const User & aUser) --should never run" << endl;
+}
+User::~User(){
+	cout << "~User(void)" << endl;
+	//user objects own playlists
+	for(int i=0; i<playlists.size(); i++)
+		delete playlists[i]; //delete playlists this user owns
+	
+}
+int User::getID(){return id;}
+
+string User::getUserID(){return userid;}
+
+vector<Playlist*>::iterator User::findPosition(Playlist & aPlaylist){
+	for (vector<Playlist*>::iterator itr = playlists.begin() ; itr != playlists.end(); ++itr)
+		if(*itr == &aPlaylist) return itr;
+	return playlists.end();
+}
+
+Playlist* User::findPlaylist(const string & aPlaylistName){
+	for (vector<Playlist*>::iterator itr = playlists.begin() ; itr != playlists.end(); ++itr)
+		if(((*itr)->getName()).compare(aPlaylistName) == 0) return *itr;
+	return NULL;
+}
+
+void User::addPlaylist(Playlist & aPlaylist){
+	//add playlist if it does not already exist
+	vector<Playlist*>::iterator itr = findPosition(aPlaylist);
+	if(itr == playlists.end()) {
+		playlists.push_back(&aPlaylist);
+	}	
+}
+
+void User::removePlaylist(Playlist & aPlaylist){
+	vector<Playlist*>::iterator itr = findPosition(aPlaylist);
+	if(itr != playlists.end()) {
+	   Playlist * playlist = *itr;
+	   playlists.erase(itr);
+	   delete playlist;
+	}
+}
+
+void User::removeTrack(Track & aTrack){
+	for (vector<Playlist*>::iterator itr = playlists.begin() ; itr != playlists.end(); ++itr){
+		Playlist * playlist = *itr;
+		playlist->removeTrack(aTrack);
+	}
+
+}
+string User::toString()const {
+	string indent = "     ";
+	string s;
+	s.append(userid + " " + name);
+	s.append("\n");
+	s.append(indent + "Playlists:\n");
+	for (vector<Playlist*>::size_type i = 0 ; i < playlists.size(); i++){
+		   s.append(indent + to_string(i) + " " + (playlists[i])->toString() + "\n");
+		   s.append("\n");
+	}
+	
+	return s;
+}
+//TODO: Could be refactored
+ostream & operator<<(ostream & out, const User & aUser){
+	out << aUser.toString() << endl;
+	return out;
+}
